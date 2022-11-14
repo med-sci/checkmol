94bd3223b26fed6272f863c6dadd2d750fe6a7d0
Yury <lupus_lucis@mail.ru>
2022-11-14 13:09:45 +0300

    Configure api

--- containers/api/main.py
+++ containers/api/main.py
@@ -1,13 +1,14 @@
+import os
 import uuid
 import requests
-from typing import Literal, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from registry import get_model_path
+from fastapi.responses import RedirectResponse
 
-
+SCORE_EVENT_LISTENER_URL = os.environ.get("SCORE_EVENT_LISTENER_URL")
 class ScoreCase(BaseModel):
     smiles: str
     protein: str
@@ -17,11 +18,18 @@ app = FastAPI()
 
 score_id = uuid.uuid4().hex
 
-@app.post(f"/items/{score_id}")
-async def score( score_case: ScoreCase, score_id: str=score_id,):
+@app.get("/")
+async def index():
+    return RedirectResponse("/docs")
+
+@app.post(f"/runs/{score_id}")
+async def score(score_case: ScoreCase, score_id: str=score_id,):
     model_path = get_model_path(score_case.protein)
-    return {
-        "score_id": score_id,
-        "model_path": model_path,
-         **score_case.dict()
-    }
+    response = requests.post(
+        SCORE_EVENT_LISTENER_URL,
+        json={
+            "smiles": score_case.smiles,
+            "scoreId": score_id,
+            "modelPath": model_path
+        })
+    return response
